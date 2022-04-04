140311b4b8f0383a6859777a4e2511628838285a
acopperlily <acopperlily@yahoo.com>
2022-04-04 15:29:02 -0400

    Refactor and add styling to contact page

--- src/contact.js
+++ src/contact.js
@@ -1,64 +1,79 @@
 const BuildContact = ((div, main, footer) => {
-    const createTitle = () => {
-        const title = document.createElement('h1');
-        title.textContent = 'Contact Us';
-        title.classList.add('pageTitle');
-        return title;
-    };
+  const createTitle = () => {
+    const title = document.createElement('h1');
+    title.textContent = 'Contact Us';
+    title.classList.add('pageTitle');
+    return title;
+  };
 
-    const createLabel = (name) => {
-        const label = document.createElement('label');
-        label.setAttribute('for', name.toLowerCase());
-        label.textContent = name;
-        return label;
+  const createLabel = (name) => {
+    const label = document.createElement('label');
+    label.setAttribute('for', name.toLowerCase());
+    label.textContent = name;
+    return label;
+  }
+
+  const createField = (field) => {
+    const input = document.createElement(field.field);
+    if (field.type) {
+      input.setAttribute('type', field.type);
     }
+    input.setAttribute('placeholder', field.name == 'Name' || field.name == 'Email' ? field.name : 'Tell us how awesome we are!');
+    input.setAttribute('id', field.name.toLowerCase());
+    return input;
+  }
 
-    const createField = (field) => {
-        const input = document.createElement(field.field);
-        if (field.type) {
-            input.setAttribute('type', field.type);
-        }
-        input.setAttribute('name', field.name.toLowerCase());
-        return input;
-    }
+  const createButton = () => {
+    const button = document.createElement('button');
+    button.textContent = 'Submit';
+    button.addEventListener('click', e => {
+      e.preventDefault();
+      alert('This is not a real form, but thanks for playing!');
+    });
+    return button;
+  }
 
-    const createForm = () => {
-        const form = document.createElement('form');
-        const fields = [
-            {
-                name: 'Name',
-                field: 'input',
-                type: 'text'
-            },
-            {
-                name: 'Email',
-                field: 'input',
-                type: 'email'
-            },
-            {
-                name: 'Message',
-                field: 'textarea',
-            }
-        ];
-        fields.forEach(field => {
-            form.appendChild(createLabel(field.name));
-            form.appendChild(createField(field));
-        });
-        const button = document.createElement('button');
-        button.textContent = 'Submit';
-        form.appendChild(button);
-        return form;
-    }
-
-    const contactFunctions = [
-        createTitle,
-        createForm
+  const createForm = () => {
+    const form = document.createElement('form');
+    const fields = [
+      {
+        name: 'Name',
+        field: 'input',
+        type: 'text'
+      },
+      {
+        name: 'Email',
+        field: 'input',
+        type: 'email'
+      },
+      {
+        name: 'Message',
+        field: 'textarea',
+      },
     ];
 
-    contactFunctions.forEach(func => main.appendChild(func()));
+    let count = 1; // For numbering div ids
+    fields.forEach(field => {
+      const div = document.createElement('div');
+      div.classList.add('userInput');
+      div.setAttribute('id', `userInput${count}`);
+      count++;
+      div.appendChild(createLabel(field.name));
+      div.appendChild(createField(field));
+      form.appendChild(div);
+    });
 
-    // main.appendChild(createTitle());
-    div.insertBefore(main, footer);
+    form.appendChild(createButton());
+    return form;
+  }
+
+  const contactFunctions = [
+    createTitle,
+    createForm
+  ];
+
+  contactFunctions.forEach(func => main.appendChild(func()));
+  div.insertBefore(main, footer);
 });
 
 export { BuildContact };
