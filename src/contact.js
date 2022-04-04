baf9d79888e9c1564c7a22fd036f2413968ee8d1
acopperlily <acopperlily@yahoo.com>
2022-04-04 05:25:18 -0400

    Add form elements to contact page

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,63 @@
 const BuildContact = ((div, main, footer) => {
     const createTitle = () => {
         const title = document.createElement('h1');
-        console.log(title);
         title.textContent = 'Contact Us';
         title.classList.add('pageTitle');
-        main.appendChild(title);
+        return title;
+    };
+
+    const createLabel = (name) => {
+        const label = document.createElement('label');
+        label.setAttribute('for', name.toLowerCase());
+        label.textContent = name;
+        return label;
     }
 
-    createTitle();
+    const createField = (field) => {
+        const input = document.createElement(field.field);
+        if (field.type) {
+            input.setAttribute('type', field.type);
+        }
+        input.setAttribute('name', field.name.toLowerCase());
+        return input;
+    }
+
+    const createForm = () => {
+        const form = document.createElement('form');
+        const fields = [
+            {
+                name: 'Name',
+                field: 'input',
+                type: 'text'
+            },
+            {
+                name: 'Email',
+                field: 'input',
+                type: 'email'
+            },
+            {
+                name: 'Message',
+                field: 'textarea',
+            }
+        ];
+        fields.forEach(field => {
+            form.appendChild(createLabel(field.name));
+            form.appendChild(createField(field));
+        });
+        const button = document.createElement('button');
+        button.textContent = 'Submit';
+        form.appendChild(button);
+        return form;
+    }
+
+    const contactFunctions = [
+        createTitle,
+        createForm
+    ];
+
+    contactFunctions.forEach(func => main.appendChild(func()));
+
+    // main.appendChild(createTitle());
     div.insertBefore(main, footer);
 });
 
